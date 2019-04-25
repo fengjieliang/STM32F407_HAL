85b16f9f56e8250e38e1000b556de9e12b79553b
FengjieLiang <fengjie.liang@beris-consulting.de>
2019-04-25 10:00:44 +0200

    UART driver

--- STM32F4_HAL/Core/Src/main.c
+++ STM32F4_HAL/Core/Src/main.c
@@ -20,11 +20,14 @@
 
 /* Includes ------------------------------------------------------------------*/
 #include "main.h"
+#include "usart.h"
 #include "gpio.h"
 
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
-
+#include <stdio.h>
+#include <string.h>
+#include "led.h"
 /* USER CODE END Includes */
 
 /* Private typedef -----------------------------------------------------------*/
@@ -45,7 +48,9 @@
 /* Private variables ---------------------------------------------------------*/
 
 /* USER CODE BEGIN PV */
-
+uint16_t USART_RX_Length=0;		//串口接收的长度
+uint8_t USART_RXBuffer[256];	//串口接收的数据保存在这
+uint8_t USART_RXSingleBuffer;
 /* USER CODE END PV */
 
 /* Private function prototypes -----------------------------------------------*/
@@ -56,6 +61,18 @@ void SystemClock_Config(void);
 
 /* Private user code ---------------------------------------------------------*/
 /* USER CODE BEGIN 0 */
+
+#ifndef __GNUC__
+#define PUTCHAR_PROTOTYPE int __io_putchar(int ch)
+#else
+#define PUTCHAR_PROTOTYPE int fputc(int ch, FILE *f)
+#endif
+//串口重定向到printf
+PUTCHAR_PROTOTYPE
+{
+    HAL_UART_Transmit(&huart1 , (uint8_t *)&ch, 1 , 0xffff);
+    return ch;
+}
 
 /* USER CODE END 0 */
 
@@ -87,8 +104,13 @@ int main(void)
 
   /* Initialize all configured peripherals */
   MX_GPIO_Init();
+  MX_USART1_UART_Init();
   /* USER CODE BEGIN 2 */
+	HAL_UART_Receive_IT(&huart1,&USART_RXSingleBuffer,1);
+	printf("Hello STM32F407\n");
 
+	bsp_led_blinken(LED1_Pin,10,300);
+	
   /* USER CODE END 2 */
 
   /* Infinite loop */
@@ -145,6 +167,46 @@ void SystemClock_Config(void)
 }
 
 /* USER CODE BEGIN 4 */
+void HAL_UART_RxCpltCallback(UART_HandleTypeDef *huart)
+{
+  /* Prevent unused argument(s) compilation warning */
+  UNUSED(huart);
+  /* NOTE: This function Should not be modified, when the callback is needed,
+           the HAL_UART_TxCpltCallback could be implemented in the user file
+   */
+	if(USART_RX_Length >= 255)  //溢出判断
+	{
+		USART_RX_Length = 0;
+		memset(USART_RXBuffer,0x00,sizeof(USART_RXBuffer));
+		//printf("数据溢出(大于256)\r\n");
+	}
+	else
+	{
+		if(USART_RX_Length==0)
+		{
+			memset(USART_RXBuffer,0,256);
+		}
+		
+		USART_RXBuffer[USART_RX_Length++] = USART_RXSingleBuffer;   //接收数据转存
+	
+		if((USART_RXBuffer[USART_RX_Length-1] == 0x0A)&&(USART_RXBuffer[USART_RX_Length-2] == 0x0D)) //判断结束位
+		{
+			USART_RX_Length=0;
+			printf("receive: %s",USART_RXBuffer);
+			if(memcmp(USART_RXBuffer,"run",3)==0)	
+			{
+				printf("\r\n start scan");
+				
+			}
+			
+			
+		}
+	}	
+	HAL_UART_Receive_IT(&huart1, (uint8_t *)&USART_RXSingleBuffer, 1);   //再开启接收中断
+}
+
+
+
 
 /* USER CODE END 4 */
 
